03a9941195b903301858626f7b452cb4a1352861
Gabriel Morales <gabriel.morales@swissbytes.ch>
2017-04-14 19:06:52 -0400

    Added first attempt to save a greenhouse

--- controllers/greenhouse.js
+++ controllers/greenhouse.js
@@ -6,5 +6,21 @@ module.exports = {
             greenhouse: []
         };
         response.render('greenhouse', viewModel);
+    },
+
+    saveGreenhouse: function (request, response) {
+        var body = request.body;
+        var greenHouse = new GreenHouseModel({
+            name: body.name,
+            state: body.state,
+            address: body.address,
+            owner: body.owner,
+            description: body.description
+        });
+
+        greenHouse.saveAll().then(function (greenhouse) {
+            response.redirect('/greenhouse/' + greenhouse.id);
+        })
+
     }
 };
--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,8 @@ var express = require('express'),
 
 module.exports = function (app, upload) {
     router.get('/', home.index);
+
     router.get('/greenhouse', greenhouse.index);
+    router.get('/savegreenhouse', greenhouse.saveGreenhouse);
     app.use(router)
 };
