31253e66c4c7762134c7cbcb5001d59b1d12246b
Gabriel Morales <gabriel.morales@swissbytes.ch>
2017-04-14 13:02:39 -0400

    Added routers and configurations to server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    config = require('./server/configuration'),
+    customConf = require('./server/customConf'),
+    configuration = require('./server/configuration'),
     app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 app.use(bodyParser());
-app.set('port', process.env.port || config.express.port);
+app.set('port', process.env.port || customConf.express.port);
+app = configuration(app);
 
 var server = app.listen(app.get('port'), function () {
     console.log('Server up: http://localhost:' + app.get('port'));
