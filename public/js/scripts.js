f8b6319a64e7a68526d769b3384a3bde01f040f2
Gabriel Morales <gabriel.morales@swissbytes.ch>
2017-04-15 18:32:46 -0400

    Adding onclick events

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,11 +6,36 @@ $(function () {
 
     $('#btn-editgreenhouse').on('click', function (event) {
         event.preventDefault();
+        var greenhouse = getGreenhouseData();
+        $('#greenhouseInformation').addClass('hide');
         $("#greenhouseForm").removeClass("hide");
+        popupateGreenhouseForm(greenhouse);
     });
 
     $('#btn-updategreenhouse').on('click', function (event) {
         event.preventDefault();
         $('#greenhouseForm').addClass("hide");
     });
+
+    function getGreenhouseData() {
+        var greenhouse = {};
+        greenhouse.state = false;
+        greenhouse.name = $('#greenhouseName').text();
+        if ($('#greenhouseActive')) {
+            greenhouse.state = true;
+        }
+        greenhouse.address = $('#greenhouseAddress').text();
+        greenhouse.owner = $('#greenhouseOwner').text();
+        greenhouse.description = $('#greenhouseDescription').text();
+        return greenhouse;
+    }
+
+    function popupateGreenhouseForm(greenhouse) {
+        $('#fNombre').val(greenhouse.name);
+        $('#fEstado').val(greenhouse.state);
+        $('#fDireccion').val(greenhouse.address);
+        $('#fDueño').val(greenhouse.owner);
+        $('#fDescripcion').val(greenhouse.description);
+    }
 });
+
