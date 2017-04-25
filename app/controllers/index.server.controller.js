746a25ad8536ab70577077eb4dafdc48da9af19e
Gabriel Morales <gabriel.morales@swissbytes.ch>
2017-04-24 23:28:33 -0400

    [GM-2] First attempt to add session management

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,5 +1,9 @@
 exports.render = function (request, response) {
+    if (request.session.lastVisit) {
+        console.log('lastVisit', request.session.lastVisit);
+    }
+    request.session.lastVisit = new Date();
     response.render('index', {
         title: 'Hello world!'
     })
-};
+};  
